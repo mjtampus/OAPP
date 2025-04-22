af972bf24841b641658130afdd246a180af7ca09
mjtampus <michaeltampus24@gmail.com>
2025-04-22 16:19:09 +0800

    Comment-UI

--- resources/views/livewire/components/product-details.blade.php
+++ resources/views/livewire/components/product-details.blade.php
@@ -155,6 +155,7 @@
             </div>
         </div>
     </div>
-
-    <livewire:components.pages-components.comment-section :productId="$product->id" />
+    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <livewire:components.pages-components.comment-section :productId="$product->id" />
     </div>
+</div>
